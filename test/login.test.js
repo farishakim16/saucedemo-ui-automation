934b7944e55f126e5da0387294e2ad2a4cd44d65
Faris Hakim <100683009+farishakim16@users.noreply.github.com>
2025-12-26 21:02:13 +0700

    push
    push

--- test/login.test.js
+++ test/login.test.js
@@ -1,42 +1,38 @@
 const { Builder, By, until } = require("selenium-webdriver");
 const assert = require("assert");
 
-describe("Login Page Tests", function () {
+describe("Inventory Sort Tests", function () {
   this.timeout(60000);
   let driver;
 
-  it("Login sukses & sort produk A-Z", async function () {
+  beforeEach(async function () {
     driver = await new Builder().forBrowser("chrome").build();
-
     await driver.get("https://www.saucedemo.com/");
+    console.log("Situs dibuka");
 
-    // login
     await driver.findElement(By.css('[data-test="username"]'))
       .sendKeys("standard_user");
+
     await driver.findElement(By.css('[data-test="password"]'))
       .sendKeys("secret_sauce");
+
     await driver.findElement(By.css('[data-test="login-button"]')).click();
 
     await driver.wait(
-      until.elementsLocated(By.css(".inventory_item_name")),
+      until.elementLocated(By.css('[data-test="product-sort-container"]')),
       10000
     );
 
-    // sort produk A-Z
     const sortDropdown = await driver.findElement(
       By.css('[data-test="product-sort-container"]')
     );
     await sortDropdown.click();
 
-    const optionAZ = await driver.findElement(
-      By.css('option[value="az"]')
-    );
-    await optionAZ.click();
+    console.log("Dropdown sort dibuka");
+  });
 
-    await driver.wait(
-      until.elementsLocated(By.css(".inventory_item_name")),
-      10000
-    );
+  it("Sort produk A-Z", async function () {
+    await driver.findElement(By.css('option[value="az"]')).click();
 
     const products = await driver.findElements(
       By.css(".inventory_item_name")
@@ -45,15 +41,37 @@ describe("Login Page Tests", function () {
     const firstName = await products[0].getText();
     const lastName = await products[products.length - 1].getText();
 
-    console.log("Produk pertama:", firstName);
-    console.log("Produk terakhir:", lastName);  
+    console.log("A-Z Produk pertama:", firstName);
+    console.log("A-Z Produk terakhir:", lastName);
+
     assert.ok(
       firstName.localeCompare(lastName) < 0,
       "Produk tidak terurut dari A ke Z"
     );
   });
 
-  after(async function () {
-    if (driver) await driver.quit();
+  it("Sort produk Z-A", async function () {
+    await driver.findElement(By.css('option[value="za"]')).click();
+
+    const products = await driver.findElements(
+      By.css(".inventory_item_name")
+    );
+
+    const firstName = await products[0].getText();
+    const lastName = await products[products.length - 1].getText();
+
+    console.log("Z-A Produk pertama:", firstName);
+    console.log("Z-A Produk terakhir:", lastName);
+
+    assert.ok(
+      firstName.localeCompare(lastName) > 0,
+      "Produk tidak terurut dari Z ke A"
+    );
+  });
+
+  afterEach(async function () {
+    if (driver) {
+      await driver.quit();
+    }
   });
 });
